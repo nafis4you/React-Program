02af8f0cb6d5163a9cfb5f5d6fca271e73a482b5
nafis4you <nafis4you@gmail.com>
2026-09-01 23:15:30 +0530

    useReducer hook

---
+++ Reducer/src/assets/component/Reducer.jsx
@@ -0,0 +1,42 @@
+import React, { useReducer } from "react";
+
+const reducer = (state, action) => {
+    if (action === "increament") {
+        return state + 1;
+    }
+
+    if (action === "decreament") {
+        return state - 1;
+    }
+
+    return state;
+};
+
+const Reducer = () => {
+
+    const [count, dispatch] = useReducer(reducer, 0);
+
+    return (
+        <div className="flex gap-4 p-5">
+
+            <button
+                onClick={() => dispatch("decreament")}
+                className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
+            >
+                Decreament
+            </button>
+
+            <span className="flex justify-center items-center">{count}</span>
+
+            <button
+                onClick={() => dispatch("increament")}
+                className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600"
+            >
+                Increament
+            </button>
+
+        </div>
+    );
+};
+
+export default Reducer;
